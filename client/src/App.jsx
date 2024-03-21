c729a962c03ad3a4d6fe29ef504b69de14198ac8
DirtyKeyboard <andrewxx000222@icloud.com>
2024-03-20 23:19:24 -0500

    init

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+import logo from "./assets/logo.png";
+import { Input, Button } from "@material-tailwind/react";
+
+function App() {
+    return (
+        <>
+            <div className="flex items-center justify-center h-screen">
+                <form className="w-[30rem] h-[45rem] rounded-3xl p-4 flex flex-col items-center shadow-inner bg-gray-100 gap-10">
+                    <img src={logo} className="w-[12rem]" />
+                    <Input
+                        label="Username"
+                        onChange={(e) => {
+                            console.log(e.target.value);
+                        }}
+                    />
+                    <Input label="Password" type="password" />
+                    <Button>Log In</Button>
+                </form>
+            </div>
+        </>
+    );
+}
+
+export default App;
